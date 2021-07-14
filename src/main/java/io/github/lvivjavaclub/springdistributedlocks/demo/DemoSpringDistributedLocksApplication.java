198a1c784eda4d928abb95778d0e04d5032342e7
Ihor Banadiga <banadiga@gmail.com>
2021-07-15 01:37:21 +0300

    Add demo

--- src/main/java/io/github/lvivjavaclub/springdistributedlocks/demo/DemoSpringDistributedLocksApplication.java
+++ src/main/java/io/github/lvivjavaclub/springdistributedlocks/demo/DemoSpringDistributedLocksApplication.java
@@ -2,10 +2,44 @@ package io.github.lvivjavaclub.springdistributedlocks.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.integration.jdbc.lock.DefaultLockRepository;
+import org.springframework.integration.jdbc.lock.JdbcLockRegistry;
+import org.springframework.integration.jdbc.lock.LockRepository;
+import org.springframework.integration.support.locks.LockRegistry;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class DemoSpringDistributedLocksApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(DemoSpringDistributedLocksApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(DemoSpringDistributedLocksApplication.class, args);
+  }
+
+//	@Bean
+//	public LockRegistry lockRegistry(RedisConnectionFactory connectionFactory) {
+//		return new RedisLockRegistry(connectionFactory, "user-lock");
+//	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+//	@Bean
+//	public LockRepository lockRepository(DataSource datasource) {
+//		return new DefaultLockRepository(datasource);
+//	}
+//
+//	@Bean
+//	public LockRegistry lockRegistry(LockRepository repository) {
+//		return new JdbcLockRegistry(repository);
+//	}
 }
--- src/main/java/io/github/lvivjavaclub/springdistributedlocks/demo/UserService.java
+++ src/main/java/io/github/lvivjavaclub/springdistributedlocks/demo/UserService.java
@@ -4,9 +4,11 @@ import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 
+import org.springframework.integration.support.locks.LockRegistry;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
+import java.util.concurrent.locks.Lock;
 
 import javax.annotation.PostConstruct;
 
@@ -15,16 +17,18 @@ import javax.annotation.PostConstruct;
 @RequiredArgsConstructor
 public class UserService {
   private final UserRepository userRepository;
-
+  
   @PostConstruct
   public void setup() {
+    log.info("===========================================================");
     log.info("User {}", userRepository.save(UserEntity.builder().balance(100L).build()));
   }
 
   @SneakyThrows(InterruptedException.class)
   public Optional<UserEntity> increaseBalance(Long id, Long add, Long sleep) {
-
     log.info("Increase balance for user {} by {}", id, add);
+
+    
     final Optional<UserEntity> userEntityById = userRepository.findById(id);
 
     if (userEntityById.isPresent()) {
@@ -35,7 +39,10 @@ public class UserService {
       long newBalance = userEntity.getBalance() + add;
 
       userEntity.setBalance(newBalance);
-      return Optional.of(userRepository.save(userEntity));
+      Optional<UserEntity> save = Optional.of(userRepository.save(userEntity));
+      log.info("UserEntity {} ", save);
+
+      return save;
     }
 
     return Optional.empty();
